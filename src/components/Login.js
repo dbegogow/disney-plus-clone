ea446bf6e1fbc1d58d7b1cc8c93dddb58de4fbbc
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-21 22:52:13 +0300

    Center text

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,32 @@ import styled from 'styled-components';
 
 const Login = (props) => {
     return (
-        <div>Login</div>
+        <Container>
+            <Content>Content</Content>
+        </Container>
     );
 };
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width: 100%;
+    possition: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+`;
 
 export default Login;
