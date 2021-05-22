99cde22ff7a8e7f271ffba74d8c38a9447fa5129
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-22 15:39:49 +0300

    Add description

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,9 @@ const Login = (props) => {
                 <CTA>
                     <CTALogoOne src="/images/cta-logo-one.svg" />
                     <SignUp>GET ALL THERE</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                    </Description>
                 </CTA>
                 <BgImage />
             </Content>
@@ -79,6 +82,14 @@ const SignUp = styled.a`
         background: #0483ee;
         cursor: pointer;
     }
+`;
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 15px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
 `;
 
 export default Login;
