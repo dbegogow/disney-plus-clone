49cd11ec2f5918555ab7eaa03a5ac77fccefe4bf
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-22 16:15:34 +0300

    Add Header component and Disney logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
 import Login from "./components/Login";
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/">
           <Login />
