69c06b9cb80059014075869773a1fc4d33d49be3
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-22 17:11:13 +0300

    Add remaining navMenus in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,26 @@ const Header = () => {
                     <img src="/images/home-icon.svg" alt="HOME" />
                     <span>HOME</span>
                 </a>
+                <a href="/home">
+                    <img src="/images/search-icon.svg" alt="SEARCH" />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/movie-icon.svg" alt="MOVIES" />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/home">
+                    <img src="/images/series-icon.svg" alt="SERIES" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
         </Nav>
     );
@@ -60,6 +80,7 @@ const NavMenu = styled.div`
         display: flex;
         align-items: center;
         padding: 0 12px;
+        text-decoration: none;
 
         img {
             height: 20px;
