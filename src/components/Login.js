3912081b4fccb2f8392fd22ff6174e94aaebb1f0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-22 15:50:43 +0300

    Add logos images

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,7 @@ const Login = (props) => {
                     <Description>
                         Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
                     </Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" />
                 </CTA>
                 <BgImage />
             </Content>
@@ -90,6 +91,14 @@ const Description = styled.p`
     margin: 0 0 24px;
     line-height: 1.5;
     letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
+    width: 100%;
 `;
 
 export default Login;
