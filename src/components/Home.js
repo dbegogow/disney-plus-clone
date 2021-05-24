98a2962bc777292d121ab9fc285a976665bea106
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-24 12:15:27 +0300

    Add background on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,31 @@ import styled from 'styled-components';
 
 const Home = (props) => {
     return (
-        <div>
-            Home
-        </div>
+        <Container>
+
+        </Container>
     );
 };
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
+
+    &:after {
+        background: url("/images/Home-background.png") center center / cover no-repeat fixed;
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        opacity: 1;
+        z-index: -1;
+    }
+`;
 
 export default Home;
