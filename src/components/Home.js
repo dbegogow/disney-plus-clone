c9a17d96bf63bb22c123928db8daae48047f1c01
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-11 23:53:44 +0300

    Add viewers  and images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import ImgSlider from './imgSlider';
+import Viewers from './Viewers';
 
 const Home = (props) => {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     );
 };
