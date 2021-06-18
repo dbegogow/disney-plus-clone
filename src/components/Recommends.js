5801a97917d71f7813996657dcda8ddb64a2afd8
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-18 13:40:49 +0300

    Add all movie categories

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectRecommend, } from '../features/movie/movieSlice';
+import { selectRecommend } from '../features/movie/movieSlice';
 
 const Recommends = () => {
     const movies = useSelector(selectRecommend);
