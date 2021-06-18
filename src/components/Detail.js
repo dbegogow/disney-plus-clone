0bdc6c0942aa431a95e907a1ec3d593ce41a083c
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-19 02:10:10 +0300

    Add trailer button in detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -22,6 +22,10 @@ const Detail = (props) => {
                         <img src="/images/play-icon-black.png" alt="" />
                         <span>Play</span>
                     </Player>
+                    <Trailer>
+                        <img src="/images/play-icon-white.png" alt="" />
+                        <span>Trailer</span>
+                    </Trailer>
                 </Controls>
             </ContentMeta>
         </Container>
@@ -122,6 +126,12 @@ const Player = styled.button`
             width: 25px;
         }
     }
+`;
+
+const Trailer = styled(Player)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
 `;
 
 export default Detail;
